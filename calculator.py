24d8dec7eea8cc05d15d53ee9390e90bf8813af4
Karan08112005 <karan.sasikala.2005@gmail.com>
2024-12-21 23:30:07 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,25 @@
-
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+print("Choose the operation:")
+print("1. Addition ")
+print("2. Subtraction ")
+print("3. Multiplication ")
+print("4. Division ")
+choice = input("Enter the number corresponding to the operation (1/2/3/4): ")
+if choice == '1':
+    result = num1 + num2
+    print(f"The result of addition is: (result)")
+elif choice == '2':
+    result = num1 - num2
+    print(f"The result of subtraction is: (result)")
+elif choice == '3':
+    result = num1 * num2
+    print(f"The result of multiplication is: (result)")
+elif choice == '4':
+    if num2 != 0:
+        result = num1 / num2
+        print(f"The result of division is: (result)")
+    else:
+        print("Error! Division by zero is not allowed.")
+else:
+    print("Invalid choice! Please choose a valid operation.")
